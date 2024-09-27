d1c512f5cddba4f3190e7a5173ee82ac940bab59
Alessandro Ricci <a.ricci@unibo.it>
2024-09-27 05:49:07 +0200

    Added first implementation - BBOM

---
+++ README.md
@@ -0,0 +1,18 @@
+#### Software Architecture and Platforms - a.y. 2023-2024
+
+## Lab #01-20240927 - [Repo](https://github.com/pslab-unibo/sap-2024-2025.git) 
+
+- Main Topic (see [Lab Notes](https://docs.google.com/document/d/1oZprsaZiSVrp5gsNEKnNra0WQ4CWwGY7xwaf7X8YiXg/edit?usp=sharing))
+	- Component & Connector (C&C) structure/view
+	- UML to represent C&C views    
+- Activity
+	- case study: Model View Controller and its extensions
+	- Source code: Labs/Lab-01   
+
+
+
+
+		
+		
+		
+		
---
+++ src/main/java/bbom/MyApp.java
@@ -0,0 +1,59 @@
+package bbom;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class MyApp extends JFrame  implements ActionListener {
+
+	private int state;
+	
+	public MyApp() {		
+		super("My BBoM App");
+
+		state = 0;
+		
+		setSize(300, 70);
+		setResizable(false);
+		
+		JButton button = new JButton("Update");
+		button.addActionListener(this);
+		
+		JPanel panel = new JPanel();
+		panel.add(button);		
+		
+		setLayout(new BorderLayout());
+	    add(panel,BorderLayout.NORTH);
+	    	    		
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent ev) {
+				System.exit(-1);
+			}
+		});
+		log("State value: " + state);
+	}
+
+	public void actionPerformed(ActionEvent ev) {
+		state++;
+		/* THIS IS THE OUTPUT */
+		log("State value: " + state);
+	}	
+	
+	private void log(String msg) {
+		System.out.println("[Output] " + msg);
+	}
+
+    static public void main(String[] args) throws Exception {			  
+    	MyApp app = new MyApp();
+		SwingUtilities.invokeAndWait(() -> {
+			app.setVisible(true);
+		});
+	}	
+}
